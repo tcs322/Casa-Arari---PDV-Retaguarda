cf608a3e50fbb6fa8bfb815fa2dbda0790f8417c
Thyerre Sampaio <thyerre322@gmail.com>
2025-08-18 11:41:24 -0300

    Refatorando view troca de senha

--- resources/views/auth/password_change.blade.php
+++ resources/views/auth/password_change.blade.php
@@ -1,12 +1,30 @@
-<form method="POST" action="{{ route('auth.password.change') }}">
+@extends('auth.layouts.auth')
+
+@section('content-auth')
+<form method="POST" action="{{ route('auth.password.change') }}" class="max-w-sm m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl">
     @csrf
-    <div>
-        <label>Nova senha</label>
-        <input type="password" name="password" required>
+    <p class="text-white font-medium text-center text-lg font-bold">ALTERAR SENHA</p>
+    @include('components.alerts.form-errors')
+
+    <div class="mt-2">
+        <label class="block text-sm text-white" for="password">Nova Senha</label>
+        <input name="password" id="password"
+            class="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
+            type="password" placeholder="Digite a nova senha" required>
     </div>
-    <div>
-        <label>Confirmar nova senha</label>
-        <input type="password" name="password_confirmation" required>
+
+    <div class="mt-2">
+        <label class="block text-sm text-white" for="password_confirmation">Confirmar Nova Senha</label>
+        <input name="password_confirmation" id="password_confirmation"
+            class="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
+            type="password" placeholder="Confirme a nova senha" required>
     </div>
-    <button type="submit">Alterar senha</button>
+
+    <div class="mt-4 flex justify-center">
+        <button type="submit"
+            class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded">
+            Alterar Senha
+        </button>
+    </div>
 </form>
+@endsection
