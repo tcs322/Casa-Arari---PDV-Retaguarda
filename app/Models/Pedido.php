bc0570be92743fa4a89d5a35da88c56c3f9f4552
Thyerre Sampaio <thyerre322@gmail.com>
2025-11-06 22:08:49 -0300

    Adicionando tabela de pedidos

---
+++ app/Models/Pedido.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido extends Model
+{
+    protected $fillable = [
+        'cliente_nome',
+        'itens',
+        'status',
+    ];
+
+    protected $casts = [
+        'itens' => 'array',
+    ];
+}
